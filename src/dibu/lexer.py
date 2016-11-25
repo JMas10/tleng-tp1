0d272472f510edb5c05291da873478fc82da0543
Jose <jmmassigoge@gmail.com>
2016-11-25 15:16:25 -0300

    Lexer

--- src/dibu/lexer.py
+++ src/dibu/lexer.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv, exit
 import ply.lex as lex
 
 """
@@ -16,19 +20,79 @@ con algún valor de ese elemento.
 
 """
 
+tokens = [
+   'NUMBER',
+   'STRING',
+   'ID',
+   'LPAREN',
+   'RPAREN',
+   'LBRACKET',
+   'RBRACKET',
+   'COMMA',
+   'EQUALS'
+]
 
-tokens = (
-)
+def t_NUMBER(token):
+    r"[0-9]+(\.[0-9]+)?"
+    if token.value.find(".") >= 0:
+        number_type = "float"
+        number_value = float(token.value)
+    else:
+        number_type = "int"
+        number_value = int(token.value)
+    token.value = {"value": number_value, "type": number_type}
+    return token
+
+def t_STRING(token):
+  r"\"([a-zA-Z_+=*-][a-zA-Z0-9_+*-]*)\""
+  return token
+
+def t_ID(token):
+    r"[a-zA-Z_+=*-][a-zA-Z0-9_+*-]*"
+    return token
+
+def t_NEWLINE(token):
+    r"\n+"
+    token.lexer.lineno += len(token.value)
+
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_LBRACKET = r"\["
+t_RBRACKET = r"\]"
+t_COMMA = r","
+t_EQUALS = r"="
+
+t_ignore = " \t"
 
 
-def t_error(t):
-    pass
+def t_error(token):
+    message = "Token desconocido:"
+    message += "\ntype:" + token.type
+    message += "\nvalue:" + str(token.value)
+    message += "\nline:" + str(token.lineno)
+    message += "\nposition:" + str(token.lexpos)
+    raise Exception(message)
 
 # Build the lexer
 lexer = lex.lex()
 
 def apply(string):
-    u"""Aplica el análisis léxico al string dado."""
+    """Aplica el análisis léxico al string dado."""
     lex.input(string)
 
     return list(lexer)
+
+if __name__ == "__main__":
+    data = '''size height=200 width=200
+    rectangle upper_left=(0,0), size=(50, 50), fill="red"
+    rectangle upper_left=(100,0), size=(50, 50)
+    rectangle upper_left=(50,50), size=(50, 50)
+    rectangle upper_left=(150,50), size=(50, 50)
+    rectangle upper_left=(0,100), size=(50, 50)
+    rectangle upper_left=(100,100), size=(50, 50)
+    rectangle upper_left=(50,150), size=(50, 50)
+    rectangle upper_left=(150,150), size=(50, 50)'''
+
+    lexer = lex.lex()
+    print(apply(data))
+    print(lexer.lineno)
